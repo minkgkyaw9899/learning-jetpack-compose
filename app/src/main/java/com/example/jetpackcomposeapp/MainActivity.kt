48172f4304cbef066af8ce121f9264916151115a
Julian Kaung <minkgkyaw9899@gmail.com>
2025-10-01 13:44:38 +0630

    feat: learning modifier

--- app/src/main/java/com/example/jetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeapp/MainActivity.kt
@@ -5,20 +5,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposeapp.ui.theme.JetpackComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,27 +47,93 @@ class MainActivity : ComponentActivity() {
 @Composable
 @Preview
 fun FirstUi(modifier: Modifier = Modifier) {
-  Box(
+  Column(
     modifier = modifier
-      .fillMaxSize()
-      .background(MaterialTheme.colorScheme.primaryContainer)
+        .fillMaxWidth()
+        .fillMaxHeight(0.5f)
+        .clip(RoundedCornerShape(24.dp))
+        .padding(24.dp)
+        .background(MaterialTheme.colorScheme.primaryContainer)
+        .verticalScroll(rememberScrollState())
   ) {
     Button(
-      modifier = modifier.align(Alignment.BottomCenter),
+      modifier = modifier.padding(20.dp),
       onClick = {}) {
       Text(text = "Button 1")
     }
 
     Button(
-      modifier = modifier.align(Alignment.TopCenter),
+      onClick = {}) {
+      Text(text = "Button 2")
+    }
+
+    Spacer(modifier = modifier.height(40.dp))
+
+    Button(
+      modifier = modifier.padding(20.dp),
+      onClick = {}) {
+      Text(text = "Button 1")
+    }
+
+    Button(
       onClick = {}) {
       Text(text = "Button 2")
     }
 
     Button(
-      modifier = modifier.align(Alignment.Center),
+      modifier = modifier.padding(20.dp),
       onClick = {}) {
-      Text(text = "Button 3")
+      Text(text = "Button 1")
     }
+
+    Button(
+      onClick = {}) {
+      Text(text = "Button 2")
+    }
+
+    Button(
+      modifier = modifier.padding(20.dp),
+      onClick = {}) {
+      Text(text = "Button 1")
+    }
+
+    Button(
+      onClick = {}) {
+      Text(text = "Button 2")
+    }
+
+    Button(
+      modifier = modifier.padding(20.dp),
+      onClick = {}) {
+      Text(text = "Button 1")
+    }
+
+    Button(
+      onClick = {}) {
+      Text(text = "Button 2")
+    }
+
+    Button(
+      modifier = modifier.padding(20.dp),
+      onClick = {}) {
+      Text(text = "Button 1")
+    }
+
+    Button(
+      onClick = {}) {
+      Text(text = "Button 2")
+    }
+
+    Button(
+      modifier = modifier.padding(20.dp),
+      onClick = {}) {
+      Text(text = "Button 1")
+    }
+
+    Button(
+      onClick = {}) {
+      Text(text = "Button 2")
+    }
+
   }
 }
