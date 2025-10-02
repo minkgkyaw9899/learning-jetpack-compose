6cd5d2867e9f6fbbf425d4e256b28f9bc7a90cf5
Julian Kaung <minkgkyaw9899@gmail.com>
2025-10-02 15:57:30 +0630

    feat: learning image remote and local

--- app/src/main/java/com/example/jetpackcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeapp/MainActivity.kt
@@ -4,59 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.text.input.TextFieldState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.rounded.Warning
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shadow
-import androidx.compose.ui.graphics.SolidColor
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import coil3.compose.AsyncImagePainter
+import coil3.compose.LocalPlatformContext
+import coil3.compose.rememberAsyncImagePainter
+import coil3.request.ImageRequest
+import coil3.size.Size
 import com.example.jetpackcomposeapp.ui.theme.JetpackComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -79,35 +59,55 @@ fun Screen() {
     horizontalAlignment = Alignment.CenterHorizontally,
     verticalArrangement = Arrangement.Center,
   ) {
-    Text(
-      text = buildAnnotatedString {
-        append("Hello, Jetpack Compose!")
-        withStyle(
-          SpanStyle(
-            color = Color.Red,
-            fontWeight = FontWeight.ExtraBold,
-            fontSize = 30.sp
-          )
-        ) {
-          append(" Welcome to the world of Android development.")
-        }
-        append(" Let's build something amazing together.")
-      },
-      fontSize = 24.sp,
-      color = MaterialTheme.colorScheme.primary,
-      fontWeight = FontWeight.Bold,
-      fontStyle = FontStyle.Italic,
-      textDecoration = TextDecoration.Underline,
-      textAlign = TextAlign.Center,
-      maxLines = 10,
-      overflow = TextOverflow.Ellipsis,
-      style = TextStyle(
-        shadow = Shadow(
-          color = Color.Gray,
-          blurRadius = 14f,
-          offset = Offset(x = 5f, y = 2f)
-        )
-      )
+    Image(
+      painter = painterResource(R.drawable.img1),
+      contentDescription = "Sample Image",
+      contentScale = ContentScale.Crop,
+      modifier = Modifier
+          .size(150.dp)
+          .clip(CircleShape)
     )
+
+    Spacer(modifier = Modifier.height(50.dp))
+
+    val imgUrl = "https://i.pinimg.com/736x/80/cc/19/80cc19f6f188b037be9f621d4c9bb189.jpg"
+
+    val model = ImageRequest
+      .Builder(LocalPlatformContext.current)
+      .size(Size.ORIGINAL)
+      .data(imgUrl)
+      .build()
+
+    val imgPainter = rememberAsyncImagePainter(model = model)
+
+    val imgState = imgPainter.state.collectAsState();
+
+    Spacer(modifier = Modifier.height(50.dp))
+
+    when (imgState.value) {
+      AsyncImagePainter.State.Empty -> Text(text = "No Image")
+      is AsyncImagePainter.State.Error -> Icon(
+        imageVector = Icons.Rounded.Warning,
+        contentDescription = null,
+        tint = Color.Red,
+        modifier = Modifier
+            .size(100.dp)
+            .width(100.dp)
+      )
+
+      is AsyncImagePainter.State.Loading -> CircularProgressIndicator(
+        modifier = Modifier.size(100.dp),
+        color = Color.Yellow
+      )
+
+      is AsyncImagePainter.State.Success -> Image(
+        painter = imgPainter,
+        contentDescription = null,
+        modifier = Modifier
+            .size(150.dp)
+            .clip(RoundedCornerShape(24.dp))
+      )
+    }
+
   }
 }
